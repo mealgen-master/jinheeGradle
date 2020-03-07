a04696ad834730ed60141b44ac34a92a1c3a7d51
ZipCock <20wlsgml@gmail.com>
2020-03-07 16:34:21 +0900

    spring test 막음

--- src/test/java/com/jinhee2/JinheeApplicationTests.java
+++ src/test/java/com/jinhee2/JinheeApplicationTests.java
@@ -1,13 +1,13 @@
-package com.jinhee2;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JinheeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.jinhee2;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JinheeApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
