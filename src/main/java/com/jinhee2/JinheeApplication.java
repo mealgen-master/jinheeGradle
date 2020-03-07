496751dafb80fece935aa84c71cd56ab90b3e867
ZipCock <20wlsgml@gmail.com>
2020-03-07 17:26:35 +0900

    war 배포를 위해 main Class 설정 추가 및 build.gradle에 war 설

--- src/main/java/com/jinhee2/JinheeApplication.java
+++ src/main/java/com/jinhee2/JinheeApplication.java
@@ -2,10 +2,17 @@ package com.jinhee2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JinheeApplication {
+public class JinheeApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JinheeApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JinheeApplication.class, args);
 	}
